b0da20441a8f3d1179115c9d68c73e9b94a98587
vanderleiromera <vanderleiromera@gmail.com>
2020-10-20 14:41:43 -0500

    [NEW] Build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+sudo apt install git gcc nano virtualenv xz-utils python3 python3-pip build-essential build-dep python3-lxml python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libpq-dev libldap2-dev libsasl2-dev libxslt1-dev libssl-dev libxml2-dev libjpeg-dev libxmlsec1-dev python3-setuptools python3-pypdf2 python3-suds python-gevent swig
+pip3 install pyldap beautifulsoup4 python-stdnum PyKCS11 \
+wget fontconfig libfreetype6 libx11-6 libxext6 libxrender1 xfonts-75dpi gitg tig
