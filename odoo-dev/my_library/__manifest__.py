bf22358431505dc34b721dae2c50c6f299ff76bc
vanderleiromera <vanderleiromera@gmail.com>
2020-10-26 14:56:06 -0500

    [NEW] Odoo-dev

---
+++ odoo-dev/my_library/__manifest__.py
@@ -0,0 +1,9 @@
+{ 
+    'name': "My library",
+    'summary': "Manage books easily",
+    'description': """Odoo development - My first App""", 
+    'author': "Vanderlei", 
+    'website': "http://www.paranstillus.com", 
+    'category': 'Uncategorized', 
+    'version': '12.0.1', 
+}
